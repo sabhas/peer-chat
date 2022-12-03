3fb5acf303593046f156af0a50ff01cc64f42aac
Sabir Hassan <sabirbk06@gmail.com>
2022-12-04 02:11:23 +0500

    add basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import React from "react"
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<h1>Lobby</h1>} />
+        <Route path="/:roomId" element={<h1>Chat room</h1>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </HashRouter>
+  )
 }
 
 export default App
